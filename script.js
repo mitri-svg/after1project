5c64c0df9b1db5daf9b4a530b5ebe016eb2bf13c
Mitri <hmirseid54@gmail.com>
2024-05-20 17:03:03 +0400

    pp

---
+++ script.js
@@ -0,0 +1,6 @@
+const sekil2 = document.querySelectorAll('.div')
+const yenisekil = document.createElement('div')
+
+yenisekil.classList.add('div')
+yenisekil.classList.add('sekil2')
+sekil2[0].after(yenisekil)
